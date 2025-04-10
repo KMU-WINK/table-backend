c215a5ea6f981f51b60bbf566b4c0be9c9dea81d
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-04-10 14:02:42 +0900

    refactor(reservation): 예약 자동반납 스케쥴링 개선

--- src/main/java/com/github/kmu_wink/domain/reservation/service/ReservationSchedulerService.java
+++ src/main/java/com/github/kmu_wink/domain/reservation/service/ReservationSchedulerService.java
@@ -5,7 +5,6 @@ import com.github.kmu_wink.domain.reservation.repository.ReservationRepository;
 import com.github.kmu_wink.domain.reservation.schema.Reservation;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -26,25 +25,26 @@ public class ReservationSchedulerService {
 
         LocalDateTime now = LocalDateTime.now();
         LocalDate today = now.toLocalDate();
-        LocalTime currentTime = now.toLocalTime();
-        LocalTime lastMinute = currentTime.minusMinutes(1);
-        LocalTime nextMinute = currentTime.plusMinutes(1);
         Set<Reservation> reservations = reservationRepository.findAllByStatusInAndDate(UPDATE_STATUS, today);
 
         reservations.forEach(reservation -> {
+            LocalDateTime startTime = LocalDateTime.of(reservation.getDate(), reservation.getStartTime());
+            LocalDateTime endTime = LocalDateTime.of(reservation.getDate(), reservation.getEndTime());
+            ReservationStatus lastStatus = reservation.getStatus();
 
-            if (reservation.getDate().isEqual(today)) {
-                if (reservation.getStatus() == ReservationStatus.PENDING &&
-                        reservation.getStartTime().isAfter(lastMinute) && currentTime.isBefore(reservation.getEndTime())) {
+            if (lastStatus == ReservationStatus.PENDING && startTime.isBefore(now)
+                    && endTime.isAfter(now)) {
 
-                    reservation.setStatus(ReservationStatus.IN_USE);
-                }
+                reservation.setStatus(ReservationStatus.IN_USE);
+            }
 
-                if (reservation.getStatus() == ReservationStatus.IN_USE && reservation.getEndTime().isAfter(lastMinute) && nextMinute.isAfter(reservation.getEndTime())) {
+            if (lastStatus == ReservationStatus.IN_USE && endTime.isBefore(now)) {
 
-                    reservation.setStatus(ReservationStatus.RETURNED);
-                }
+                reservation.setStatus(ReservationStatus.RETURNED);
+            }
 
+
+            if (!lastStatus.equals(reservation.getStatus())) {
                 reservationRepository.save(reservation);
             }
         });
