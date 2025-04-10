9dd9c180e58d711486a91c706ca0008a9060fab3
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-11 03:00:37 +0900

    chore(reservation): 스케줄링 패키지 분리

---
+++ src/main/java/com/github/kmu_wink/domain/reservation/util/schedule/ReservationScheduler.java
@@ -0,0 +1,62 @@
+package com.github.kmu_wink.domain.reservation.util.schedule;
+
+import static com.github.kmu_wink.domain.reservation.constant.ReservationStatus.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.github.kmu_wink.domain.reservation.constant.ReservationStatus;
+import com.github.kmu_wink.domain.reservation.repository.ReservationRepository;
+import com.github.kmu_wink.domain.reservation.schema.Reservation;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ReservationScheduler {
+
+    private final ReservationRepository reservationRepository;
+
+    private final Set<ReservationStatus> UPDATE_STATUS = Set.of(PENDING, IN_USE);
+
+    @PostConstruct
+    public void onStartUp() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        reservationRepository.findAllByStatusIn(UPDATE_STATUS)
+            .forEach((reservation) -> updateReservationStatus(reservation, now));
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void cron() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        reservationRepository.findAllByStatusInAndDate(UPDATE_STATUS, now.toLocalDate())
+            .forEach((reservation) -> updateReservationStatus(reservation, now));
+    }
+
+    private void updateReservationStatus(Reservation reservation, LocalDateTime current) {
+
+        LocalDateTime startTime = LocalDateTime.of(reservation.getDate(), reservation.getStartTime());
+        LocalDateTime endTime = LocalDateTime.of(reservation.getDate(), reservation.getEndTime());
+
+        if (reservation.getStatus() == PENDING && current.isAfter(startTime)) {
+
+            reservation.setStatus(IN_USE);
+            reservationRepository.save(reservation);
+        }
+
+        if (reservation.getStatus() == IN_USE && current.isAfter(endTime)) {
+
+            reservation.setStatus(RETURNED);
+            reservation.setReturnedAt(LocalDateTime.now());
+            reservationRepository.save(reservation);
+        }
+    }
+}
